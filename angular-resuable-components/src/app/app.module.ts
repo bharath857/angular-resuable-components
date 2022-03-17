3103af39ba0bf600954223896f58e0b8599fa0ce
bharath857 <bharath.reddy.857@gmail.com>
2022-03-17 21:47:39 +0530

    basic_structure

--- angular-resuable-components/src/app/app.module.ts
+++ angular-resuable-components/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
