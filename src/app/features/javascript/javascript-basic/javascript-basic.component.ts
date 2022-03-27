ff682093ce4f8303a799684a211b19e6a1f313c4
bharath857 <bharath.reddy.857@gmail.com>
2022-03-28 01:35:54 +0530

    separted concepts with links

--- src/app/features/javascript/javascript-basic/javascript-basic.component.ts
+++ src/app/features/javascript/javascript-basic/javascript-basic.component.ts
@@ -7,11 +7,17 @@ import links from '../../../core/constants/links'
   styleUrls: ['./javascript-basic.component.css']
 })
 export class JavascriptBasicComponent implements OnInit {
-  listOfConcepts:string[] = ['String','String','String'];
-  listOfStringMentods:string[] = ['charAt()','charCodeAt','concat(string1, string2, ...)','includes()', 
+
+  listOfConcepts:string[] = ['String','Array','String'];
+  concept:string = '';
+  
+  listOfStringMentods:string[] = ['charAt()','charCodeAt()','concat(string1, string2, ...)','includes()', 
   'endsWith()', 'indexOf()', 'lastIndexOf()','match()','matchAll()', 'repeat()', 'replace()', 'replaceAll()', 
   'search()','spice()','split()','startsWith()', 'substr()', 'substring()', 'toLowerCase()', 'toUpperCase()', 'toString()','trim()', 'valueOf()'];
-  
+
+  selectedString:string = 'charAt()';
+  showAllStringMenthods:boolean = false;
+
   javaScript_basic:string;
 
   constructor() {
