6a23fa02edeff3e9cd12801d5a2cde28a68dc771
bharath857 <bharath.reddy.857@gmail.com>
2022-03-28 13:07:36 +0530

    added all array methods and console methods

--- src/app/features/javascript/javascript-basic/javascript-basic.component.ts
+++ src/app/features/javascript/javascript-basic/javascript-basic.component.ts
@@ -8,24 +8,34 @@ import links from '../../../core/constants/links'
 })
 export class JavascriptBasicComponent implements OnInit {
 
-  listOfConcepts:string[] = ['String','Array','String'];
-  concept:string = '';
-  
-  listOfStringMentods:string[] = ['charAt()','charCodeAt()','concat(string1, string2, ...)','includes()', 
-  'endsWith()', 'indexOf()', 'lastIndexOf()','match()','matchAll()', 'repeat()', 'replace()', 'replaceAll()', 
-  'search()','spice()','split()','startsWith()', 'substr()', 'substring()', 'toLowerCase()', 'toUpperCase()', 'toString()','trim()', 'valueOf()'];
+  listOfConcepts: string[] = ['String', 'Array', 'Console'];
+  concept: string = '';
 
-  selectedString:string = 'charAt()';
-  showAllStringMenthods:boolean = false;
+  //String
+  listOfStringMentods: string[] = ['charAt()', 'charCodeAt()', 'concat(string1, string2, ...)', 'includes()',
+    'endsWith()', 'indexOf()', 'lastIndexOf()', 'match()', 'matchAll()', 'repeat()', 'replace()', 'replaceAll()',
+    'search()', 'spice()', 'split()', 'startsWith()', 'substr()', 'substring()', 'toLowerCase()', 'toUpperCase()', 'toString()', 'trim()', 'valueOf()'];
+  selectedString: string = 'charAt()';
+  showAllStringMenthods: boolean = false;
+  javaScript_basic: string;
 
-  javaScript_basic:string;
+  //array
+  listOfArrayMentods: string[] = ['concat()','copyWithIn()','entries()','every()','fill()','filter()','find()','findIndex()',
+  'forEach()','Array.from()','includes()','indexOf()','isArray()','join()','lastIndexOf()','map()',
+  'push()','pop()','shift()','unshift()','reduce()','reverse()','slice()','some()','sort()','splice()','toString()','values()'];
+  selectedArray: string = 'charAt()';
+  showAllArrayMenthods: boolean = false;
 
+  //Console
+  listofConsoleMethods: string[] = ['console.log()', 'console.assert()', 'console.clear()', 'console.count()', 'console.dir()', 'console.error()', 'console.table()', 'console.time()'];
+  selectedConsole: string = 'console.log()';
+  showAllConsoleMenthods: boolean = false;
   constructor() {
     this.javaScript_basic = links.weblinks.javascript_basic;
-   }
+  }
 
   ngOnInit(): void {
-    
+
   }
 
 }
