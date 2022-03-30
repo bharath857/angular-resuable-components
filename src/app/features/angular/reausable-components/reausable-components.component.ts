8cddff075da33ee18d3002086af0ede044d0a2b6
bharath857 <bharath.reddy.857@gmail.com>
2022-03-30 20:30:27 +0530

    Adjusted postion and input values

--- src/app/features/angular/reausable-components/reausable-components.component.ts
+++ src/app/features/angular/reausable-components/reausable-components.component.ts
@@ -16,6 +16,8 @@ export class ReausableComponentsComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
     this.popUpIncludes = this.formBuilder.group({
       showHeader: [true],
+      headerValue:['Heading'],
+      footerValue:['footer'],
       showFooter: [false],
       footerHeading: [false],
       showAsPopUp: [true],
@@ -30,10 +32,10 @@ export class ReausableComponentsComponent implements OnInit {
     this.showPopUp = false;
     setTimeout(() => {
       this.popUpdetails = {
-        headerOne: 'Heading of PopUp ',
+        headerOne: this.popUpIncludes.get('headerValue')?.value,
         showHeader: this.popUpIncludes.get('showHeader')?.value,
         showFooter: this.popUpIncludes.get('showFooter')?.value,
-        footerHeading: this.popUpIncludes.get('footerHeading')?.value ? 'footer heading' : '',
+        footerHeading: this.popUpIncludes.get('footerHeading')?.value ? this.popUpIncludes.get('footerValue')?.value : '',
         showAsPopUp: this.popUpIncludes.get('showAsPopUp')?.value,
         confirmation: this.popUpIncludes.get('comfirmation')?.value,
         size: this.size,
